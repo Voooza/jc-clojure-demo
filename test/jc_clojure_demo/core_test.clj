6e001f1a105625865da004c11caf0280da372d8d
voooza <petr@vanis.org>
2016-11-07 20:51:10 +0100

    project import

---
+++ test/jc_clojure_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns jc-clojure-demo.core-test
+  (:require [clojure.test :refer :all]
+            [jc-clojure-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
